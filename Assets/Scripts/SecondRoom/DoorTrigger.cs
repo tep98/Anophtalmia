0145995e5dbc925cb6b0a1f6d5a229e8d6edcce2
Zaklzzzy <zaletin123@gmail.com>
2024-03-11 00:53:57 +0300

    fix: fix second level

--- Assets/Scripts/SecondRoom/DoorTrigger.cs
+++ Assets/Scripts/SecondRoom/DoorTrigger.cs
@@ -39,23 +39,23 @@ public class DoorTrigger : MonoBehaviour
     {
         if (_type == Type.Front)
         {
-            _player.position = new Vector3(_player.position.x, _player.position.y, _player.position.z + 12.5f);
-            _virtualCamera.position = new Vector3(_virtualCamera.position.x, _virtualCamera.position.y, _virtualCamera.position.z + 25f);
+            _player.position = new Vector3(_player.position.x, _player.position.y, _player.position.z + 6f);
+            _virtualCamera.position = new Vector3(_virtualCamera.position.x, _virtualCamera.position.y, _virtualCamera.position.z + 12f);
         }
         else if (_type == Type.Left)
         {
-            _player.position = new Vector3(_player.position.x - 12.5f, _player.position.y, _player.position.z);
-            _virtualCamera.position = new Vector3(_virtualCamera.position.x - 25f, _virtualCamera.position.y, _virtualCamera.position.z);
+            _player.position = new Vector3(_player.position.x - 6f, _player.position.y, _player.position.z);
+            _virtualCamera.position = new Vector3(_virtualCamera.position.x - 12f, _virtualCamera.position.y, _virtualCamera.position.z);
         }
         else if (_type == Type.Back)
         {
-            _player.position = new Vector3(_player.position.x, _player.position.y, _player.position.z - 12.5f);
-            _virtualCamera.position = new Vector3(_virtualCamera.position.x, _virtualCamera.position.y, _virtualCamera.position.z - 25f);
+            _player.position = new Vector3(_player.position.x, _player.position.y, _player.position.z - 6f);
+            _virtualCamera.position = new Vector3(_virtualCamera.position.x, _virtualCamera.position.y, _virtualCamera.position.z - 12f);
         }
         else if (_type == Type.Right)
         {
-            _player.position = new Vector3(_player.position.x + 12.5f, _player.position.y, _player.position.z);
-            _virtualCamera.position = new Vector3(_virtualCamera.position.x + 25f, _virtualCamera.position.y, _virtualCamera.position.z);
+            _player.position = new Vector3(_player.position.x + 6f, _player.position.y, _player.position.z);
+            _virtualCamera.position = new Vector3(_virtualCamera.position.x + 12f, _virtualCamera.position.y, _virtualCamera.position.z);
         }
     }
 }
