e4e94f96520d4512e53ce9ddd355321f00597e17
tep98 <Korotkovstepan95@gmail.com>
2024-03-12 01:25:21 +0300

    feat: add sound design to final and prologue scenes

--- Assets/Scripts/Enable Object.cs
+++ Assets/Scripts/Enable Object.cs
@@ -5,11 +5,19 @@ using UnityEngine;
 public class EnableObject : MonoBehaviour
 {
     [SerializeField] private ParticleSystem _particles;
+    [SerializeField] private AudioSource _shotGunShot;
+    [SerializeField] private AudioSource _shotGunReload;
     [SerializeField] private GameObject _object;
 
-    public void PlayParticles()
+    public void PlayShot()
     {
         _particles.Play();
+        _shotGunShot.Play();
+    }
+
+    public void PlayReload()
+    {
+        _shotGunReload.Play();
     }
 
     public void ObjectSetActive()
