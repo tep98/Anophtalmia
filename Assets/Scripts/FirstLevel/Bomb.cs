b347559d7b8a0e8d0ce2e57c0e08062e16ee3514
Zaklzzzy <zaletin123@gmail.com>
2024-03-11 10:13:54 +0300

    feat: add bomb pickup

--- Assets/Scripts/FirstLevel/Bomb.cs
+++ Assets/Scripts/FirstLevel/Bomb.cs
@@ -4,13 +4,15 @@ using UnityEngine;
 public class Bomb : MonoBehaviour
 {
     [SerializeField] private GameObject _canvas;
+    [SerializeField] private GameObject _bombPlace;
+    [SerializeField] private GameObject _hand;
 
     private bool _explosed = false;
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.E) && _explosed)
+        if (Input.GetKeyDown(KeyCode.E))
         {
-            //exlosive
+            _hand.SetActive(true);
             _canvas.SetActive(false);
         }
     }
